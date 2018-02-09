07238eea25e133046534dbfe08de7060214b3b1b
Dmitriy Gryn <lkibbalam@lkibbalam-lx.nixsolutions.com>
2018-02-09 18:53:29 +0200

    refactor test for ADD method

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,6 +1,5 @@
 # create a tree structure
 class Tree
-  attr_reader :first_node
   def initialize
     @first_node = Node.new('*')
   end
--- spec/spec_prefix_tree.rb
+++ spec/spec_prefix_tree.rb
@@ -5,9 +5,9 @@ RSpec.describe Tree do
   it 'it return keys of child arrays' do
     tree = Tree.new
     tree.add('abc')
-    expect(tree.first_node.key).to eq('*')
-    expect(tree.first_node.array[0].key).to eq('a')
-    expect(tree.first_node.array[0].array[0].key).to eq('b')
-    expect(tree.first_node.array[0].array[0].array[0].key).to eq('c')
+    expect(tree.instance_variable_get(:@first_node).key).to eq('*')
+    expect(tree.instance_variable_get(:@first_node).array[0].key).to eq('a')
+    expect(tree.instance_variable_get(:@first_node).array[0].array[0].key).to eq('b')
+    expect(tree.instance_variable_get(:@first_node).array[0].array[0].array[0].key).to eq('c')
   end
 end
